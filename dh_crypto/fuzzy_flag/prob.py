0e459384b18623db9ff908aabaa7e813a79035cc
purplesky <purplesky@korea.ac.kr>
2024-03-05 17:45:02 +0900

    organization

--- dh_crypto/fuzzy_flag/prob.py
+++
@@ -1,9 +0,0 @@
-from secrets import randbelow
-import string
-
-with open('flag', 'rb') as f:
-    flag = f.read()
-    
-fuzzy = [c + randbelow(len(string.ascii_letters)) for c in flag]
-
-print(fuzzy)
